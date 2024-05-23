8b2e74f68ee81f656b122760cb47f3d48b7f1c90
KOBUKOVU <nickgiatao126@gmail.com>
2024-05-24 00:16:13 +0700

    final version

--- nmapScanPort.py
+++ nmapScanPort.py
@@ -1,44 +1,83 @@
+#import nmap for scanning 
 import nmap 
-import re 
+#import regular expression for the valid ip address 
+import re
+#import ipaddress to check the validity of the input IP address
+import ipaddress
+# import print-color module for a better CLI view
+from print_color import print
 
-ipAddressPattern = re.compile("^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$")
-portRangePattern = re.compile("([0-9]+)-([0-9]+)")
+#use the regular expression to extract the number of ports user want to scan 
+# specify <lowest> - <highest> port number
 
-portMin = 0
-portMax = 65535
+port_range_pattern = re.compile("([0-9]+)-([0-9]+)")
 
-print("==============================")
-print("Welcome to the port scanner!!!")
-print("______________________________")
-print("Designed by Clay")
-print("==============================")
-openPort = []
+#initiate the port numbers
 
+port_min = 0; 
+port_max = 65535
+
+def display_header():
+    print("**************************************")
+    print("*        ____ _                      *")
+    print("*       / ___| | __ _ _   _          *")
+    print("*      | |   | |/ _` | | | |         *")
+    print("*      | |___| | (_| | |_| |         *")
+    print("*       \____|_|\__,_|\__, |         *")
+    print("*                     |___/          *")
+    print("*                                    *")
+    print("*            WELCOME TO              *")
+    print("*        Port Scanner Program        *", color="blue")
+    print("**************************************")
+    print("*                                    *")
+    print("*     -Only use for right purpose-   *")
+    print("*                                    *")
+    print("**************************************")
+    print("*           Designed by Clay         *")
+    print("**************************************")
+
+# Call the function to display the header
+display_header()
+
+
+#take the input ip 
 while True: 
-    ipAddressEntered = input("\nEnter the ip address: ")
-    if ipAddressPattern.search(ipAddressEntered): 
-        print(f"{ipAddressEntered} is valid")
-        break 
+    print("\nWhich ip address you want to scan? ")
+    ip_address_entered = input("Enter the ip address: ")
+# check the validity of the ipaddress 
+    try: 
+        #check the validity with the ipaddress module
+        ip_address_valid = ipaddress.ip_address(ip_address_entered)
+        print("VALID ip address", tag ="succes", tag_color="green", color = "white")
+        break
+    except: 
+        print("INVALID ipaddress", tag ="failure", tag_color="red", color = "white")
 
+#take the port range
 while True: 
-    print("Enter the range of ports you want to scan(between 20-100): ")
-    portRange = input("Enter port range(firstPort - lastPort): ")
-    portRangeValid = portRangePattern.search(portRange.replace(" ",""))
-    if portRangeValid: 
-        portMin = int(portRangeValid.group(1))
-        portMax = int(portRangeValid.group(2))
+    #Can scan port from 0-65535
+    print("Enter the range of ports that you want to scan (<int> - <int>)")
+    port_range = input("Enter the range: ")
+    #cut off the space between two numbers: 
+    port_range_valid = port_range_pattern.search(port_range.replace(" ",""))
+    if port_range_valid: 
+        #find the lowest port 
+        port_min = int(port_range_valid.group(1))
+        #find the highest port
+        port_max = int(port_range_valid.group(2))
         break
 
-nm = nmap.PortScanner()
-
-for port in range(portMin, portMax + 1): 
+nmap = nmap.PortScanner()
+#go through all the port in the range 
+for port in range (port_min, port_max + 1):
     try: 
-        result = nm.scan(ipAddressEntered, str(port))
-        portStatus = (result['scan'][ipAddressEntered]['tcp'][port]['state'])
-        if portStatus == 'open':
-            print(f"\033[1;32;40m Port: {port} is {portStatus}")
-            
+        result = nmap.scan(ip_address_entered, str(port))
+        # extract port status from the returned object 
+        port_status = (result["scan"][ip_address_entered]["tcp"][port]["state"])
+        if port_status == "open":
+            print(f"Port {port} is {port_status}", color = "green")
         else: 
-            print(f"\033[1;31;40mPort: {port} is {portStatus}")
-    except: 
-        print("Cannot scan port")
+            print(f"Port {port} is {port_status}", color = "magenta")
+    except:    
+        print(f"Cannot scan port {port}.", color = "red")
+    
